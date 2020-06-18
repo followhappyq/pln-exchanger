bddcad77e6537334976ea75b3af2247d362fc8b0
Ivan <followhappyq@gmail.com>
2020-06-18 15:17:26 +0200

    Updates

---
+++ precache-manifest.09adfca4b31e15f4348654666f04e458.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5dd55968a49831545468c08a1eeb46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23ba9e2d305745c6ddab",
+    "url": "/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "4e41de9aa17e7ef85377",
+    "url": "/static/css/main.909562c9.chunk.css"
+  },
+  {
+    "revision": "23ba9e2d305745c6ddab",
+    "url": "/static/js/2.43881c3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.43881c3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e41de9aa17e7ef85377",
+    "url": "/static/js/main.f4031703.chunk.js"
+  },
+  {
+    "revision": "73371153185df6948439",
+    "url": "/static/js/runtime-main.ffade4ea.js"
+  }
+]);
