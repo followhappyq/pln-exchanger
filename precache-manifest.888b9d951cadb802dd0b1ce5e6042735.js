935423a0c70cda3147550a1be1422ff2d6054063
Ivan <followhappyq@gmail.com>
2020-06-18 17:30:53 +0200

    Updates

---
+++ precache-manifest.888b9d951cadb802dd0b1ce5e6042735.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40e399ce9943c3ced8a77eed6d03ebc",
+    "url": "/pln-exchanger/index.html"
+  },
+  {
+    "revision": "ed8385728e1712f98f33",
+    "url": "/pln-exchanger/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "0f69ba35b672ed72065e",
+    "url": "/pln-exchanger/static/css/main.00987acb.chunk.css"
+  },
+  {
+    "revision": "ed8385728e1712f98f33",
+    "url": "/pln-exchanger/static/js/2.db18e20f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pln-exchanger/static/js/2.db18e20f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f69ba35b672ed72065e",
+    "url": "/pln-exchanger/static/js/main.86a3517e.chunk.js"
+  },
+  {
+    "revision": "61d877cce2951e30cab1",
+    "url": "/pln-exchanger/static/js/runtime-main.973afe0d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pln-exchanger/precache-manifest.061def8de6efb88e982d3c191a064322.js"
+  "/pln-exchanger/precache-manifest.888b9d951cadb802dd0b1ce5e6042735.js"
 );
 
 self.addEventListener('message', (event) => {
